45718e4f43d66fd8b06c66b21cb05eeafe682fca
xuseindumbadze <xuseindumbadze1@gmail.com>
2026-02-08 22:02:16 +0400

    update

---
+++ ფუნქციები/function.js
@@ -0,0 +1,117 @@
+// let num1 = 7.45,
+//   num2 = "13.56";
+
+// num2 = Number(num2);
+
+// num1 = Math.round(num1);
+// num2 = Math.round(num2);
+
+// let result = num1 * num2;
+
+// document.write(result);
+
+
+
+
+
+
+// let num1 = 20.3;
+// let num2 = 27.2;
+
+// let result = num1 + num2;
+
+// document.write(result.toFixed(2));
+
+// let num = 77;
+
+// let result = Math.sqrt(num);
+
+// result = result.toFixed(1);
+
+// console.log(result);
+
+
+
+
+
+
+
+// let num = 3.55;
+// let result = Math.pow(num, 3);
+
+// result = result.toFixed(2);
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+// function info(text) {
+//   alert(text);
+// }
+
+// info("გამარჯობა, ეს არის ტექსტი");
+
+
+
+
+
+
+
+
+// function exponentiation(base, power) {
+//   let result = Math.pow(base, power);
+//   alert(result);
+//   return result;
+// }
+// exponentiation(2, 5);
+
+
+
+
+
+
+
+
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+// console.log(isEven(8));  
+// console.log(isEven(5)); 
+
+
+
+
+
+
+
+// function getStartPart(text, count) {
+//   return text.slice(0, count);
+// }
+// console.log(getStartPart("JavaScript", 4)); 
+// console.log(getStartPart("HelloWorld", 5)); 
+
+
+
+
+
+
+// function address(addr) {
+//   document.getElementById("my-Address").innerHTML += "<br>" + addr;
+// }
+// let userAddress = prompt("შეიყვანეთ თქვენი მისამართი:");
+
+// address(userAddress);
+
+
+
+
+
+
