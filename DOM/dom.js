ec05f1a3f2a8ec3cef2d74ef196f697354819f66
xuseindumbadze <xuseindumbadze1@gmail.com>
2026-02-11 23:55:01 +0400

    update

---
+++ DOM/dom.js
@@ -0,0 +1,109 @@
+// const MyButton = document.getElementById("button")
+// console.log(MyButton)
+
+// const paragraph1 = document.querySelector("#p1");
+// const paragraph2 = document.querySelector("#p2");
+// const paragraph3 = document.querySelector("#p3");
+
+// console.log(paragraph1);
+// console.log(paragraph2);
+// console.log(paragraph3);
+
+// const twoParagraphs = document.querySelector("#twoParagraphs p")
+// console.log(twoParagraphs)
+
+// const firstParagraph = document.querySelector(".p");
+// console.log(firstParagraph);
+
+// const button_1 =document.getElementById("button_1")
+// const button_2 =document.getElementById("button_2")
+// const button_3 =document.getElementById("button_3")
+
+// button_1.addEventListener("click", () =>{
+//     alert("პირველი ღილაკი")
+// });
+
+// button_2.addEventListener("click", () =>{
+//     alert("მეორე ღილაკი")
+// });
+
+// button_3.addEventListener("click", () =>{
+//     alert("მესამე ღილაკი")
+// });
+
+// const button_1 = document.getElementById("button_1");
+// const button_2 = document.getElementById("button_2");
+// const button_3 = document.getElementById("button_3");
+
+// button_1.addEventListener("click", function sum() {
+//   console.log(2 + 2);
+// });
+
+// button_2.addEventListener("click", function multiplication() {
+//   console.log(3 * 3);
+// });
+
+// button_3.addEventListener("click", function divide() {
+//  console.log(4 / 4);
+//  });
+
+// const elem1 = document.getElementById("elem1");
+// const elem2 = document.getElementById("elem2");
+// const elem3 = document.getElementById("elem3");
+
+// elem1.addEventListener("click", function myFunc() {
+//   console.log("საინტერესო შეტყობინება");
+// });
+
+// elem2.addEventListener("click", function myFunc() {
+//   console.log("საინტერესო შეტყობინება");
+// });
+
+// elem3.addEventListener("click", function myFunc() {
+//   console.log("საინტერესო შეტყობინება");
+// });
+
+// const button = document.getElementById("button")
+
+// function myFunc1() {
+//   alert('საინტერესო შეტყობინება');
+// }
+
+// function myFunc2() {
+//   console.log('სუპერ ღილაკზე მოხდა დაკლიკება');
+// }
+
+// button.addEventListener("click", function() {
+//   myFunc1();
+//   myFunc2();
+// });
+
+// const a = document.getElementById("myText");
+
+// function myFunc1() {
+//   alert("შეტყობინება1");
+// }
+// function myFunc2() {
+//   alert("შეტყობინება2");
+// }
+// function myFunc3() {
+//   alert("შეტყობინება3");
+// }
+
+// a.addEventListener("click", function(){
+//     myFunc1();
+//     myFunc2();
+//     myFunc3();
+// })
+
+// const btn = document.getElementById("button");
+
+// btn.addEventListener("dblclick", function () {
+//   alert("სუპერ ღილაკზე მოხდა ორმაგი დაკლიკება");
+// });
+
+// const btn = document.getElementById("button");
+
+// btn.addEventListener("mouseover", function () {
+//   alert("სუპერ ღილაკზე მოხდა მაუსის გადატარება");
+// });
